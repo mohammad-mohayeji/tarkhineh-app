52d81978ece304f3a6dbc7de0ba663ae6107c9ed
Mohammad_Mohayeji <mohammadmohayeji80@gmail.com>
2023-12-10 19:13:37 +0330

    update

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay } from "swiper/modules";
 
 import slider1 from "../assets/images/slider.png";
 import slider2 from "../assets/images/slider2.png";
 import slider3 from "../assets/images/slider3.png";
+import testSlider from "../assets/images/testSlider.png"
 
-// import icons
-import { ArrowLeft2, ArrowRight2 } from "iconsax-react";
+// import custom navigation buttons
+import SliderNavBtns from "./SliderNavBtns";
 
 const divStyle = {
   height: "380px",
@@ -24,42 +33,22 @@ const divStyle = {
 const slideImages = [
   { url: slider1, caption: "تجربه غذای سالم و گیاهی به سبک ترخینه" },
   { url: slider2, caption: "طعم بی نظیر طبیعت" },
-  { url: slider3, caption: "لذت غذای سالم و گیاهی را با ترخینه تجربه کنید‌" },
+  { url: testSlider, caption: "لذت غذای سالم و گیاهی را با ترخینه تجربه کنید‌" },
 ];
-
-const buttonStyle = {
-  width: "30px",
-  background: "none",
-  border: "0px",
-};
-
-const properties = {
-  prevArrow: (
-    <button style={{ ...buttonStyle }} className="ml-4">
-      <ArrowLeft2 className="text-white" />
-    </button>
-  ),
-  nextArrow: (
-    <button style={{ ...buttonStyle }} className="mr-4">
-      <ArrowRight2 className="text-white" />
-    </button>
-  ),
-};
 
 export default function MainSlider() {
   return (
-    <div className="slide-container">
-      <Slide {...properties} duration={3000}>
-        {slideImages.map((slideImage, index) => (
-          <div key={index}>
+    <Swiper autoplay={{ delay: 3000, disableOnInteraction: false }} modules={[Autoplay]} className="mySwiper relative">
+      {slideImages.map((slideImage, index) => (
+          <SwiperSlide key={index}>
             <div style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}>
               <span className="text-white text-sm sm:text-xl lg:text-3xl font-medium sm:font-bold">
                 {slideImage.caption}
               </span>
             </div>
-          </div>
-        ))}
-      </Slide>
-    </div>
+          </SwiperSlide>
+      ))}
+      <SliderNavBtns />
+    </Swiper>
   );
 }
---
+++ src/components/SliderNavBtns.js
@@ -0,0 +1,17 @@
+import { useSwiper } from "swiper/react";
+import { ArrowLeft2, ArrowRight2 } from "iconsax-react";
+
+export default function SliderNavBtns() {
+  const swiper = useSwiper();
+
+  return (
+    <div className="w-full flex justify-between items-center gap-x-4 absolute top-1/2 -translate-y-1/2 z-20">
+      <button onClick={(e) => swiper.slidePrev()}>
+        <ArrowRight2 className="w-8 h-8 text-white mr-4" />
+      </button>
+      <button onClick={(e) => swiper.slideNext()}>
+        <ArrowLeft2 className="w-8 h-8 text-white ml-4" />
+      </button>
+    </div>
+  );
+}
